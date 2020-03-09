f47ecf2f386950d78e5b051553bb996d6ccb4c56
David Cortarello <david.cortarello@accenture.com>
2020-03-08 23:10:04 -0300

    Implemented dynamic news

---
+++ js/news.js
@@ -0,0 +1,39 @@
+
+$(document).ready( function() {
+
+	function get_new_content(link) {
+		$.ajax(link)
+			.then( function (new_rawdata) {
+				return new_rawdata;
+			}
+			.fail( function (new_err) {
+				return "";
+			}
+	}
+
+
+	var news_per_page = 5;
+	var urls = window.location.href.split("/");
+	var idx = urls[urls["length"]-1].replace('.html', '').replace('.htm', '').replace('news', '');
+	show_news = news_per_page * idx;
+
+	$("#loader").hide();
+
+	$.ajax("content/news/index.json")
+		.then( function (rawdata) {
+			var nIndex = JSON.parse(rawdata);
+			var sorter_nIndex = nIndex.sort(function(a, b) { return b.date - a.date; } );
+			var news_content = "";
+			for(i = 0; i < news_per_page; i++) {
+				var new_data = sorter_nIndex[show_news + i];
+				if (new_data != undefined) {
+					news_content += "<br>" + get_new_content(new_data.link);
+				}
+			}
+			$("#newsplaceholder").html(news_content);
+		} )
+		.fail (function (rawdata) {
+			$("#newsplaceholder").html("<p>Can't read any news");
+		} )
+} )
+
