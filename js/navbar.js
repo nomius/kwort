16003f3ea4294837601a3caba6fe13eb3ec90ba1
David Cortarello <david.cortarello@accenture.com>
2020-03-22 10:05:35 -0300

    Ooops, typo!

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ $.get("navbar.html", function(data){
 	$("#nav-placeholder").replaceWith(data);
 	var link = document.URL.split("/").pop();
 	links = document.getElementsByTagName("a");
-	for (i = 0; i < links.lenght; i++) {
+	for (i = 0; i < links.length; i++) {
 		if (links[i].href.pop() == link) {
 			links[i].className = "nav-link active";
 		}
