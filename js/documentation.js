2f9e51dad48ea8be37483e71bda473c9dd935bb0
David Cortarello <david.cortarello@accenture.com>
2020-03-22 01:50:22 -0300

    Documentation is now fully dynamic with markdown and marked as parser and render

---
+++ js/documentation.js
@@ -0,0 +1,19 @@
+$(document).ready( function() {
+	var section = document.URL.split("?")[1]
+	if (section != null) {
+		var active_item = document.getElementById(section)
+		active_item.className = active_item.className + " active"
+	}
+	else {
+		section = "documentation"
+	}
+
+	$.ajax("content/documentation/" + section + ".md")
+		.then( function (data) {
+			data_section = document.getElementsByClassName("col-lg-8")[0].innerHTML = marked(data)
+		} )
+		.fail( function (rawdata) {
+			document.getElementsByClassName("col-lg-8")[0].innerHTML = "<p>Documentation not accessible";
+		} )
+} )
+
