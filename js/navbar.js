5f59ae2ad13036d7e2f23b21eb935d59c830693a
David Cortarello <david.cortarello@accenture.com>
2020-03-22 10:07:18 -0300

    You need to split man!

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ $.get("navbar.html", function(data){
 	var link = document.URL.split("/").pop();
 	links = document.getElementsByTagName("a");
 	for (i = 0; i < links.length; i++) {
-		if (links[i].href.pop() == link) {
+		if (links[i].href.split("/").pop() == link) {
 			links[i].className = "nav-link active";
 		}
 	}
