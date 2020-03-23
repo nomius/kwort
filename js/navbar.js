fb4ddb75fd085a517fb23d6fb431702709cea993
David Cortarello <david.cortarello@accenture.com>
2020-03-22 22:50:00 -0300

    make index empty so the navigation bar behaves the same across all pages

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,9 @@
 $.get("navbar.html", function(data){
 	$("#nav-placeholder").replaceWith(data);
 	var link = document.URL.split("?")[0].split("/").pop()
+	if (link == "index.html") {
+		link = ""
+	}
 	links = document.getElementsByTagName("a");
 	for (i = 0; i < links.length; i++) {
 		if (links[i].href.split("/").pop() == link) {
