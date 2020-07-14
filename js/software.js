d9964b91ed42fb05a475efaa7094787d18f9f69e
David Cortarello <david.cortarello@accenture.com>
2020-07-13 23:21:31 -0300

    Typo

--- js/software.js
+++ js/software.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 	function create_table(packages) {
 		packages.sort(function (a, b) {
 			if (a['epoch'] < b['epoch']) {
-				return 1
+				return 1;
 			}
 			else {
 				return -1;
@@ -47,13 +47,13 @@ $(document).ready(function() {
 		out = head;
 		for (i=0; i < (packages.length < 20 ? packages.length : 20); i++) {
 			if (packages[i].size > 10485760) { // More than 10MB
-				out_size = Math.ceil(packages[i].size / 1048576) + "mb"
+				out_size = Math.ceil(packages[i].size / 1048576) + "mb";
 			}
 			else {
-				out_size = Math.ceil(packages[i].size/1024) + "kb"
+				out_size = Math.ceil(packages[i].size/1024) + "kb";
 			}
 			d = Date(packages[i].epoch).split(" ");
-			out = out + "<tr><td>" + packages[i].name + "</td><td>" + packages[i].version + '</td"><td style="text-align: center;">' + packages[i].build + "</td><td>" + d[0] + " " + d[1] + " " + d[2] + " " + d[3] + 	</td><td>" + out_size+ "</td></tr>";
+			out = out + "<tr><td>" + packages[i].name + "</td><td>" + packages[i].version + '</td"><td style="text-align: center;">' + packages[i].build + "</td><td>" + d[0] + " " + d[1] + " " + d[2] + " " + d[3] + "</td><td>" + out_size+ "</td></tr>";
 		}
 		out = out + bottom;
 		$("#loader").hide();
