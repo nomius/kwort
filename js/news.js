24dd69fccb123498787f54fd2aaefe85e3ee7205
David Cortarello <david.cortarello@accenture.com>
2020-12-26 19:03:40 -0300

    Second try

--- js/news.js
+++ js/news.js
@@ -1,4 +1,15 @@
-$(document).ready( function() {
+$(document).ready(function() {
+
+	function makeAjaxCall(url, parameters, methodType, callback){
+		return $.ajax({
+			url : url,
+			cache: true,
+			method : methodType,
+			data : parameters,
+			dataType : "json",
+			contentType : 'application/json'
+		});
+	}
 
 	var news_per_page = 5;
 	var urls = window.location.href.split("/");
@@ -9,24 +20,18 @@ $(document).ready( function() {
 
 	$("#loader").hide();
 
-	$.ajax("content/news/index.json")
-		.then( function (nIndex) {
-			var sorter_nIndex = nIndex.sort(function(a, b) { return b.date - a.date; } );
-			if (sorter_nIndex.length < news_per_page) {
-				to_show = sorter_nIndex.length;
-			}
-			else {
-				to_show = news_per_page;
-			}
-			for(i = 0; i < to_show; i++) {
-				var new_item = sorter_nIndex[show_news + i];
-				$.ajax(new_item.link)
-					.then(function (new_content) {
-						$("#newsplaceholder").append(marked(new_content) + "<br>");
-					} )
-			}
-		} )
-		.fail (function (rawdata) {
-			$("#newsplaceholder").html("<p>Can't read any news");
-		} )
-} )
+	makeAjaxCall("content/news/index.json", null, "GET").then(function(nIndex){
+		if (nIndex.length < news_per_page) {
+			to_show = nIndex.length;
+		}
+		else {
+			to_show = news_per_page;
+		}
+		for(i = 0; i < to_show; i++) {
+			var new_item = sorter_nIndex[show_news + i];
+			makeAjaxCall(new_item.link, null, "GET").then(function(content) {
+				$("#newsplaceholder").append(marked(new_content) + "<br>");
+			});
+		}
+	});
+});
