dd272af4c6caadbc6373dfaab8dca9d98f750b33
nomius <dcortarello@gmail.com>
2024-07-21 12:37:24 -0300

    monthIndex starts at 0, not 1

--- js/software.js
+++ js/software.js
@@ -5,7 +5,7 @@ function str2date(sdate) {
 	year = sdate.split(" ")[0].split("-")[2];
 	hour = sdate.split(" ")[1].split(":")[0];
 	minute = sdate.split(" ")[1].split(":")[1];
-	all_months = { 'Jan' : 1, 'Feb' : 2, 'Mar' : 3, 'Apr' : 4, 'May' : 5, 'Jun' : 6, 'Jul' : 7, 'Aug': 8, 'Sep' : 9, 'Oct' : 10, 'Nov' : 11, 'Dec' : 12 }
+	all_months = { 'Jan' : 0, 'Feb' : 1, 'Mar' : 2, 'Apr' : 3, 'May' : 4, 'Jun' : 5, 'Jul' : 6, 'Aug': 7, 'Sep' : 8, 'Oct' : 9, 'Nov' : 10, 'Dec' : 11 }
 	month = all_months[smonth];
 	return new Date(year, month, day, hour, minute);
 }
